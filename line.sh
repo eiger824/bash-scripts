36600dbbf0701a28be8d4ef659cee2c2695768ce
esapago <santiago.pagola@ericsson.com>
2018-08-09 15:11:29 +0200

    Extra checks

--- line.sh
+++ line.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+is_nr()
+{
+    if [[ $1 =~ [0-9]+ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 usage()
 {
     echo "Usage: $(basename $0) <fname> <line-nr>" >&2
@@ -9,7 +18,8 @@ usage()
 fname=$1
 linenr=$2
 
-test -z "$fname" && usage
+! is_nr $linenr && usage
+test ! -f "$fname" && usage
 
 sed $linenr'q;d' $fname
 
